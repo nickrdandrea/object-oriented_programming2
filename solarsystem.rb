69adace251b17811e7e2e00ddd18eac8b315b971
nickrdandrea <nickrdandrea@gmail.com>
2017-01-22 20:09:18 -0500

    Complete solar system

--- solarsystem.rb
+++ solarsystem.rb
@@ -8,17 +8,17 @@ class System
     @bodies << body
   end
 
-  def total_mass(mass)
+  def total_mass()
     total = 0
     @bodies.each do |i|
-      total += @bodies[i]
+      total += i.mass.to_i
     end
     total
   end
 end
 
 class Body
-  attr_reader: :name, :mass
+  attr_reader :name, :mass
   def initialize(name, mass)
     @name = name
     @mass = mass
@@ -26,24 +26,36 @@ class Body
 end
 
 class Planet < Body
-  attr_reader :day, :year
-  def initialize(day, year)
+  attr_accessor :day, :year
+  def initialize(day, year, name, mass)
     @day = day
     @year = year
+    super(name, mass)
   end
 end
 
 class Star < Body
-  attr_accessor :type
-  def initialize(type)
+  attr_accessor :type, :name, :mass
+  def initialize(type, name, mass)
     @type = type
+    super(name, mass)
   end
 end
 
 class Moon < Body
   attr_accessor :month, :planet
-  def initialize(month, planet)
+  def initialize(month, planet, name, mass)
     @month = month
     @planet = planet
+    super(name, mass)
   end
 end
+
+sol = System.new()
+sun = Star.new("G-type","Sun",1000)
+sol.add(sun)
+earth = Planet.new(24, 365, "Earth", 100)
+sol.add(earth)
+moon = Moon.new(27, earth, "Moon", 10)
+sol.add(moon)
+puts "#{sol.total_mass}"
